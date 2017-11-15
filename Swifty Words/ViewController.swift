7c869ace579745aab1958282f882ddf70fb39047
Mirza <mirza@github.com>
2017-11-15 18:16:29 +0100

    Initial Commit

---
+++ Swifty Words/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swifty Words
+//
+//  Created by Mirza Durakovic on 15/11/2017.
+//  Copyright © 2017 Mirza Durakovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
